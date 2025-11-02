a2f81d0662f6025d05587c3f64d98146afd6c579
kartikeydino <kartikeydino@gmail.com>
2025-11-02 13:24:38 -0800

    fixed jump_count glitch

--- scripts/player.gd
+++ scripts/player.gd
@@ -14,11 +14,14 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		
 	if is_on_floor():
 		jump_count = 0
+		
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and jump_count < max_jumps:
 		velocity.y = JUMP_VELOCITY
+		jump_count += 1
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
