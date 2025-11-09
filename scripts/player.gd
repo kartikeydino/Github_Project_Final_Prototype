f73f09016f742d396a28fe0b7c2817a42b0c73eb
kartikeydino <kartikeydino@gmail.com>
2025-11-09 13:14:42 -0800

    started on gun script
    added inputs e and left_mouse

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,18 @@
 extends CharacterBody2D
 
+@onready var sprite = $Sprite2D
+@onready var gun = $gun
 
 const SPEED = 230.0
 const JUMP_VELOCITY = -300.0
 var jump_count = 0
 var max_jumps = 2
+
+var bullet = preload("res://scenes/bullet.tscn")
+var gun_equipped = false
+var gun_cooldown = false
+var gun_direction = null
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -26,6 +34,14 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
+	
+	if direction > 0 and gun_equipped:
+		gun.flip_h = false
+		gun.position = Vector2(14,3)
+	elif direction < 0 and gun_equipped:
+		gun.flip_h = true
+		gun.position = Vector2(-14,3)
+		
 	if direction:
 		velocity.x = direction * SPEED
 	else:
@@ -33,6 +49,21 @@ func _physics_process(delta):
 
 	move_and_slide()
 
+	if Input.is_action_just_pressed("e"):
+		if gun_equipped:
+			gun_equipped = false
+			gun.visible = false
+		else:
+			gun_equipped = true
+			gun.visible = true
+	
+	if Input.is_action_just_pressed("left_mouse") and gun_equipped and gun_cooldown:
+		gun_cooldown = false
+		var bullet_instance = bullet.instantiate()
+		add_child(bullet_instance)
+		
+		await get_tree().create_timer(0.5).timeout
+		gun_cooldown = true
 
 func player():
 	pass
