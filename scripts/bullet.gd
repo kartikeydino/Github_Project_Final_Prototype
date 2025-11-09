a891c6a4830db3fdddb326638d686c7dce42ae5a
kartikeydino <kartikeydino@gmail.com>
2025-11-09 13:19:04 -0800

    bullet script

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,1 +1,14 @@
 extends Area2D
+
+var speed = 300
+
+func _ready():
+	pass
+	
+func _process(delta):
+	pass
+	
+
+
+func _on_visible_on_screen_enabler_2d_screen_exited():
+	pass # Replace with function body.
